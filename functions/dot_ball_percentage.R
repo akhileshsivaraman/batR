c535de27a715fea20b193d85eb96071c8716d7e6
Akhilesh Sivaraman <66359910+akhileshsivaraman@users.noreply.github.com>
2023-09-28 16:06:28 +0100

    Delete functions directory
    directory deleted to get remote repo ready for merging with local repo

--- functions/dot_ball_percentage.R
+++
@@ -1,8 +0,0 @@
-#---- calculate dot ball % ----
-dot_ball_percentage <- function(ball_by_ball_data){
-  balls <- ball_by_ball_data[3:ncol(ball_by_ball_data)]
-  dot_ball_count <- sum(balls == 0, na.rm = T)
-  balls_faced <- sum(balls, na.rm = T)
-  dot_ball_percent <- round(dot_ball_count/balls_faced*100, 2)
-  return(dot_ball_percent)
-}
--- functions/find_initials.R
+++
@@ -1,5 +0,0 @@
-#---- Find initials ----
-find_initials <- function(player_to_find){
-  d <- find_player_id(player_to_find) |>
-    select(c("Name", "Country", "Played"))
-}
