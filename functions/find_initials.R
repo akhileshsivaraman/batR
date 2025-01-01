9af6021cea31b4c772dd400ea0f0e943beb325ed
akhileshsivaraman <akhileshsiva@aol.com>
2025-01-01 18:32:30 +0000

    Added roxygen comments

--- functions/find_initials.R
+++ functions/find_initials.R
@@ -1,4 +1,9 @@
-#---- Find initials ----
+#' Find initials for a player of interest
+#' 
+#' Performs matching to a supplied player name to identify potential record matches in the form of initials + surname.
+#' 
+#' @param player_to_find - string: name of a player
+
 find_initials <- function(player_to_find){
   d <- find_player_id(player_to_find) |>
     select(c("Name", "Country", "Played"))
