a64fd42d6b0d3f535dc9b51610cd32b4b5b29208
akhileshsivaraman <akhileshsiva@aol.com>
2025-04-09 13:07:37 +0100

    modified variable names for consistency

--- tests/testthat/test-career_bbb.R
+++ tests/testthat/test-career_bbb.R
@@ -1,15 +1,15 @@
 test_that("condense_career returns a dataframe with 1 row", {
-  d <- read_rds(test_path("fixtures", "womens_innings_list.RDS"))
-  x <- condense_career(d[[1]])
+  innings_list_data <- read_rds(test_path("fixtures", "womens_innings_list.RDS"))
+  x <- condense_career(innings_list_data[[1]])
   
   expect_s3_class(x, "data.frame")
   expect_equal(nrow(x), 1)
 })
 
 test_that("the length of the dataframe returned by condense career - 2 = n_rows of the df supplied - wides", {
-  d <- read_rds(test_path("fixtures", "mens_innings_list.RDS"))
+  innings_list_data <- read_rds(test_path("fixtures", "mens_innings_list.RDS"))
   
-  df_supplied <- d[[1]]
+  df_supplied <- innings_list_data[[1]]
   n_wides <- df_supplied |>
     filter(!is.na(wides)) |>
     tally() |>
@@ -22,10 +22,10 @@ test_that("the length of the dataframe returned by condense career - 2 = n_rows 
 
 
 test_that("career_bbb returns a dataframe with n_rows equal to the length of the list supplied", {
-  d <- read_rds(test_path("fixtures", "mens_innings_list.RDS"))
+  innings_list_data <- read_rds(test_path("fixtures", "mens_innings_list.RDS"))
   
-  x <- career_bbb(d)
+  ball_by_ball_data <- career_bbb(d)
   
-  expect_s3_class(x, "data.frame")
-  expect_equal(nrow(x), length(d))
+  expect_s3_class(ball_by_ball_data, "data.frame")
+  expect_equal(nrow(ball_by_ball_data), length(innings_list_data))
 })
